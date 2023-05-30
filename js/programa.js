9cdf3a451b17888c956910fde2cb8741c12fa5ee
aledelorbe <mgranadosb1800@alumno.ipn.mx>
2023-05-30 17:36:44 -0600

    cargador del mismo tema entre diferentes paginas

--- js/programa.js
+++ js/programa.js
@@ -3,18 +3,51 @@ $(document).ready(function(){
 	// SELECCION DE TEMA
 	var theme = $("#theme");
 
+	// Cambia el tema y guarda cual tema es en el localStorage.
 	$("#to-green").click(function(){
 		theme.attr("href", "../css/temas/green.css");
+
+		if( typeof(Storage) !== 'undefined' ) {
+			console.log('LocalStorage Disponible');
+			localStorage.setItem("tema", "../css/temas/green.css");
+		} else {
+			console.log('LocalStorage NO Disponible');
+		}
 	});
 	
 	$("#to-red").click(function(){
 		theme.attr("href", "../css/temas/red.css");
+
+		if( typeof(Storage) !== 'undefined' ) {
+			console.log('LocalStorage Disponible');
+			localStorage.setItem("tema", "../css/temas/red.css");
+		} else {
+			console.log('LocalStorage NO Disponible');
+		}
 	});
 	
 	$("#to-blue").click(function(){
 		theme.attr("href", "../css/temas/blue.css");
+
+		if( typeof(Storage) !== 'undefined' ) {
+			console.log('LocalStorage Disponible');
+			localStorage.setItem("tema", "../css/temas/blue.css");
+		} else {
+			console.log('LocalStorage NO Disponible');
+		}
 	});
 	
+	// Carga el tema que se haya seleecionado por la anterior pagina
+	var tipoTema = localStorage.getItem("tema");
+
+	if(tipoTema == null){
+		theme.attr("href", "../css/temas/green.css");
+	}
+	else{
+		theme.attr("href", tipoTema);
+	}
+
+
 	// BOTON PARA IR ARRIBA
 	$('.subir').click(function(e){
 		e.preventDefault();
