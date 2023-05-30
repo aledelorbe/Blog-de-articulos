1856a6930e44612d4154781249e83b86b4f85fc7
aledelorbe <mgranadosb1800@alumno.ipn.mx>
2023-05-29 19:12:02 -0600

    perfeccionamiento de reloj y separacion de estilos

--- js/programaReloj.js
+++ js/programaReloj.js
@@ -4,7 +4,22 @@ $(document).ready(function(){
 	// Programacion que unicamente debe funcionar en la pagina del reloj.
 	setInterval(function(){
 		let fecha = new Date();
-		var reloj = fecha.getHours() + ':' + fecha.getMinutes() + ':' + fecha.getSeconds();
+
+		var hora = fecha.getHours();
+		var esAM = hora < 12; // Verificar si es AM (hora menor a 12)
+
+		// Convertir la hora a formato de 12 horas
+		if (hora > 12) {
+		hora = hora - 12;
+		} else if (hora === 0) {
+		hora = 12;
+		}
+
+		var minutos = fecha.getMinutes();
+		var segundos = fecha.getSeconds();
+		var amPm = esAM ? "AM" : "PM";
+
+		var reloj = hora.toString().padStart(2, '0') + ':' + minutos.toString().padStart(2, '0') + ':' + segundos.toString().padStart(2, '0') + ' ' + amPm;
 		$('#reloj').html(reloj);
 	}, 1000);
 });
